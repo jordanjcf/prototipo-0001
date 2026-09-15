1d99592e2eee7d457f3ef79f46fbeb79350b3e86
YesseyAG <tannyyessey@gmail.com>
2026-09-14 21:39:24 -0500

    cambie nombre de grupo a  JUGADORES

--- Script/salida.gd
+++ Script/salida.gd
@@ -4,5 +4,5 @@ func _ready():
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
-	if body.is_in_group("Player"):
+	if body.is_in_group("Jugadores"):
 		get_tree().change_scene_to_file("res://Scenes/desierto.tscn")
