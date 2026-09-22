235f537f3ee72b9246850f2c19abf08887d59e53
jordanjcf <josset_96@hotmail.es>
2026-09-22 17:36:50 -0500

    cambios

--- Script/player.gd
+++ Script/player.gd
@@ -1,16 +1,21 @@
 extends CharacterBody2D
 
-
 const SPEED = 120
 const JUMP_VELOCITY = -350
 
 @onready var animation = $AnimatedSprite2D
+var atacando = false
 
 func _physics_process(delta: float) -> void:
 	# esta es mi gravedad
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
+	#atacar
+	if Input.is_action_pressed("ata") :
+		atacando = true
+		animation.play("atacar")
+	
 	# este es para saltar
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -31,4 +36,7 @@ func _physics_process(delta: float) -> void:
 		animation.play("caminar")
 	else:
 		animation.play("descanso")
-		
+	
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	pass # Replace with function body.
