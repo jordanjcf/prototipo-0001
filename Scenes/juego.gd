0bd98b9edf863d6273413fcc88da309a7192df18
jordanjcf <josset_96@hotmail.es>
2026-09-23 11:21:03 -0500

    cambios

--- Scenes/juego.gd
+++ Scenes/juego.gd
@@ -8,40 +8,30 @@ func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
+func cambiar_jugador(playera,playerb):
+	var posicion = playera.global_position
+	playera.visible = false
+	playera.process_mode = Node.PROCESS_MODE_DISABLED
+	playera.get_node("CollisionShape2D").disabled = true
+	playera.get_node("Camera2D").enabled = false
+	
+	playerb.global_position = posicion
+	playerb.visible = true
+	playerb.process_mode = Node.PROCESS_MODE_INHERIT
+	playerb.get_node("CollisionShape2D").disabled = false
+	playerb.get_node("Camera2D").enabled = true
 
 func _on_boton_player_b_pressed():
-	var posicion = player.global_position
-	
-	player.visible = false
-	player.process_mode = Node.PROCESS_MODE_DISABLED
-	$Jugador/Player/CollisionShape2D.disabled = true
-	$Jugador/Player/Camera2D.enabled = false
-
-	player_b.global_position = posicion
-	player_b.visible = true
-	player_b.process_mode = Node.PROCESS_MODE_INHERIT
-	$Jugador/PlayerB/CollisionShape2D.disabled = false
-	$Jugador/PlayerB/Camera2D.enabled = true
+	cambiar_jugador(player, player_b)
 	
 	$MenuJugador/Panel/Opciones/BotonPlayer.visible = true
 	$MenuJugador/Panel/Opciones/BotonPlayerB.visible = false
 
 func _on_boton_player_pressed():
-	var posicion = player_b.global_position
-	
-	player_b.visible = false
-	player_b.process_mode = Node.PROCESS_MODE_DISABLED
-	$Jugador/PlayerB/CollisionShape2D.disabled = true
+	cambiar_jugador(player_b, player)
 
-	player.global_position = posicion
-	player.visible = true
-	player.process_mode = Node.PROCESS_MODE_INHERIT
-	$Jugador/Player/CollisionShape2D.disabled = false
-	$Jugador/Player/Camera2D.enabled = true
-	$Jugador/PlayerB/Camera2D.enabled = false
-	
 	$MenuJugador/Panel/Opciones/BotonPlayer.visible = false
 	$MenuJugador/Panel/Opciones/BotonPlayerB.visible = true
